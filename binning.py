1b8d08430111a50cbf4947898987fb6a0db75828
Vinit <vinithorakeri@gmail.com>
2023-12-07 09:51:02 -0500

    test changes to branch

--- binning.py
+++ binning.py
@@ -1,6 +1,8 @@
 #Numerical Binning Example
+import pandas as pd
+
+
 data['bin'] = pd.cut(data['value'], bins=[0,30,70,100], labels=["Low", "Mid", "High"])
- import pandas as pd
 
 
 #reading file
@@ -11,3 +13,5 @@ bins = [1,19,30,50,100]
 labels = ['minor','young','old','very_old']
 
 df_bin['age_range'] = pd.cut(df_bin['age'],bins = bins, labels = labels)
+##New change
+
